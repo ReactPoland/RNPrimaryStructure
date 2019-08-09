44893123bcc5b07f34d5aede3786798063c5b258
AdamStrzempa <adams@reactpoland.com>
2019-08-09 17:21:02 +0200

    validate data

--- src/network/api/cocktails.js
+++ src/network/api/cocktails.js
@@ -1,8 +1,18 @@
 import _get from 'lodash/get';
+import _isArray from 'lodash/isArray';
+import _isPlainObject from 'lodash/isPlainObject';
 import api from './index';
+
+const dataValidator = data => {
+  const isArray = _isArray(data)
+  if (!isArray) throw Error('Data is not valid');
+  const isWrongObject = data.some(item => !_isPlainObject(item))
+  if (isWrongObject) throw Error("Data is not valid");
+};
 
 export const getData = async () => {
   const response = await api.getCocktailGlassData()
   const data = _get(response, 'data.drinks', [])
+  dataValidator(data)
   return data
 };
