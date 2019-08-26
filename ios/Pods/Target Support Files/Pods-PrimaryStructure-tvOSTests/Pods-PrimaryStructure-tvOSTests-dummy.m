8862210db0291da5eec922328e61237f9995ce7e
Łukasz Kubica <kubicalukasz92@gmail.com>
2019-08-26 14:52:02 +0200

    init commit

---
+++ ios/Pods/Target Support Files/Pods-PrimaryStructure-tvOSTests/Pods-PrimaryStructure-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrimaryStructure_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_PrimaryStructure_tvOSTests
+@end
---
+++ src/screens/Home/index.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux'
+import Home from './Home'
+
+const mapDispatchToProps = {}
+
+const mapStateToProps = ({ session }) => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
