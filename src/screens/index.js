bb5f612b22cf43c3124ce7d919959d1f25f864a2
AdamStrzempa <adams@reactpoland.com>
2019-09-05 16:51:05 +0200

    update react navigation, add react-navigation-stack

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,21 +1,22 @@
-import {
-    createStackNavigator,
-    createAppContainer
-  } from 'react-navigation'
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
 import HomeScreen from './Home'
 import TestsScreen from './Tests'
 
-const MainNavigator = createStackNavigator({
-  Home: HomeScreen,
-  Tests: TestsScreen
-}, {
-  navigationOptions: {
-    headerTintColor: 'black',
-    headerStyle: {
-      backgroundColor: '#1D213C'
-    }
-  }
-})
+const MainNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Tests: TestsScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: 'black',
+      headerStyle: {
+        backgroundColor: 'white',
+      },
+    },
+  },
+)
 
 const App = createAppContainer(MainNavigator)
 
